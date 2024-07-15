510fbeab21310d6c4211c13ee9e48693362f0907
jacantwell <jasper66018@gmail.com>
2024-07-15 14:35:35 +0100

    adding conftest to s3 unit tests

--- tests/s3/unit/test_load_string.py
+++ tests/s3/unit/test_load_string.py
@@ -19,30 +19,6 @@ def string_response():
     return mocked_response
 
 
-@pytest.fixture
-def no_such_bucket_response():
-    error_response = {
-        "Error": {
-            "Code": "NoSuchBucket",
-            "Message": "The specified bucket does not exist",
-        }
-    }
-    operation_name = "GetObject"
-    return ClientError(error_response, operation_name)
-
-
-@pytest.fixture
-def no_such_key_response():
-    error_response = {
-        "Error": {
-            "Code": "NoSuchKey",
-            "Message": "The specified key does not exist",
-        }
-    }
-    operation_name = "GetObject"
-    return ClientError(error_response, operation_name)
-
-
 def test_success(string_response):
     config = {"bucket": "test-bucket"}
     storage = S3Storage(config)
